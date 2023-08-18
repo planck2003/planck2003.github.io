6350c8b7f3087814750853bef17b6aaa18c38ef3
planck2003 <2732916179@qq.com>
2023-08-18 11:45:20 +0800

    Site updated: 2023-08-18 11:45:20

--- js/utils.js
+++ js/utils.js
@@ -1397,7 +1397,7 @@ const executeLoad = (config, target) => {
 
 const config = {
   smallSrc: 'https://img01.anheyu.com/useruploads/243/2023/07/31/64c7d0b9d6e73.jpg', // 小图链接 尽可能配置小于100k的图片
-  largeSrc: 'https://t.mwm.moe/pc', // 大图链接 最终显示的图片
+  largeSrc: 'https://api.dearxuan.com/image?type=pc', // 大图链接 最终显示的图片
   mobileSmallSrc: 'https://img01.anheyu.com/useruploads/243/2023/07/31/64c7d0b9d6e73.jpg', // 手机端小图链接 尽可能配置小于100k的图片
   mobileLargeSrc: 'https://t.mwm.moe/mp', // 手机端大图链接 最终显示的图片
   enableRoutes: ['/'],
