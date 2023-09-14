98d9e21d88f92038b933ed3e47167c2d50208dbf
planck2003 <2732916179@qq.com>
2023-09-14 18:22:28 +0800

    Site updated: 2023-09-14 18:22:28

--- js/utils.js
+++ js/utils.js
@@ -1392,7 +1392,7 @@ const executeLoad = (config, target) => {
 
 const config = {
   smallSrc: 'https://img01.anheyu.com/useruploads/243/2023/08/18/64df5fbca3853.webp', // 小图链接 尽可能配置小于100k的图片
-  largeSrc: 'https://img01.anheyu.com/useruploads/243/2023/08/31/64f0625b2ec1e.webp', // 大图链接 最终显示的图片
+  largeSrc: 'https://img01.anheyu.com/useruploads/243/2023/08/03/64cb2d535bebc.webp', // 大图链接 最终显示的图片
   mobileSmallSrc: 'https://img01.anheyu.com/useruploads/243/2023/07/31/64c7d0b9d6e73.jpg', // 手机端小图链接 尽可能配置小于100k的图片
   mobileLargeSrc: 'https://t.mwm.moe/mp', // 手机端大图链接 最终显示的图片
   enableRoutes: ['/'],
